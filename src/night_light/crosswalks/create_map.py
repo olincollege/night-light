f7920f15fb82fbfc399c08a6cbcd579d31e9cd07
Daeyoung Kim <dkim@cellinobio.com>
2024-10-30 20:50:29 -0400

    Add more descriptive docstrings

--- src/night_light/crosswalks/create_map.py
+++ src/night_light/crosswalks/create_map.py
@@ -5,7 +5,24 @@ import geopandas as gpd
 def create_folium_map(
     gdf: gpd.GeoDataFrame, center: list, zoom_start: int, map_filename: str
 ):
-    """Create a Folium map with the given GeoDataFrame and save it to an HTML file."""
+    """
+    Create a Folium map from a GeoDataFrame and save it to an HTML file.
+
+    This function generates a Folium map centered at a specified location and zoom
+    level, overlays the geometries from the provided GeoDataFrame, and saves the map as
+    an HTML file.
+
+    Args:
+        gdf (gpd.GeoDataFrame): A GeoDataFrame containing geometries to be added to the
+        map.
+        center (list): A list containing the latitude and longitude for the map center
+        [latitude, longitude].
+        zoom_start (int): The initial zoom level for the map.
+        map_filename (str): The file path where the HTML map will be saved.
+
+    Returns:
+        None
+    """
     m = folium.Map(location=center, zoom_start=zoom_start)
     folium.GeoJson(
         gdf,
--- src/night_light/crosswalks/query_geojson.py
+++ src/night_light/crosswalks/query_geojson.py
@@ -3,7 +3,25 @@ import geopandas as gpd
 
 
 def fetch_geojson_data(url: str, params: dict) -> gpd.GeoDataFrame:
-    """Fetch GeoJSON data from the given URL with specified parameters."""
+    """
+    Fetch GeoJSON data from a specified URL with given parameters.
+
+    This function sends a GET request to the provided URL with the specified query
+    parameters, retrieves the GeoJSON data, and converts it into a GeoDataFrame with an
+    EPSG:4326 CRS.
+
+    Args:
+        url (str): The URL to request the GeoJSON data from.
+        params (dict): A dictionary of query parameters to include in the request.
+
+    Returns:
+        gpd.GeoDataFrame: A GeoDataFrame containing the geometries and properties from
+        the fetched GeoJSON data.
+
+    Raises:
+        requests.HTTPError: If the HTTP request fails.
+        ValueError: If the retrieved data does not contain valid GeoJSON features.
+    """
     response = requests.get(url, params=params)
     response.raise_for_status()
     try:
@@ -16,5 +34,14 @@ def fetch_geojson_data(url: str, params: dict) -> gpd.GeoDataFrame:
 
 
 def save_geojson(gdf: gpd.GeoDataFrame, filename: str):
-    """Save GeoDataFrame to a GeoJSON file."""
+    """
+    Save a GeoDataFrame to a GeoJSON file.
+
+    Args:
+        gdf (gpd.GeoDataFrame): The GeoDataFrame to be saved as GeoJSON.
+        filename (str): The file path where the GeoJSON will be saved.
+
+    Returns:
+        None
+    """
     gdf.to_file(filename, driver="GeoJSON")
