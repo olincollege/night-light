a0306dbb2d4f86f4afe1397837fd9dc729b9f8fb
Daeyoung Kim <dkim@cellinobio.com>
2025-03-05 16:29:23 -0500

    Update table name

--- src/night_light/GIS_predictor/percieved_brightness.py
+++ src/night_light/GIS_predictor/percieved_brightness.py
@@ -2,14 +2,18 @@ def calculate_percieved_brightness(conn):
     """
     Calculate percieved brightness by adding A + B hueristic together.
     """
-    conn.execute("""
-    ALTER TABLE crosswalk_centers_classified_lights 
+    conn.execute(
+        """
+    ALTER TABLE crosswalk_centers_contrast 
     ADD COLUMN IF NOT EXISTS light_heuristic FLOAT;
-    """)
+    """
+    )
 
-    conn.execute("""
-    UPDATE crosswalk_centers_classified_lights
-    SET light_heuristic = from_side_heuristic + to_side_heuristic;
-    """)
-    
+    conn.execute(
+        """
+    UPDATE crosswalk_centers_contrast
+    SET light_heuristic = from_heuristic + to_heuristic;
+    """
+    )
+
     print("Light heuristic calculated")
